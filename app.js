17ba3a70f3bb5e638d74a668e3385daf8aad498d
Sajjad Ali <157633197+aleysajjad@users.noreply.github.com>
2024-05-11 15:02:13 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,100 @@
+// Question Number 01
+
+// var a = prompt("Enter Your Name")
+
+var name = "Rizwan Jamal";
+console.log(name.toLowerCase());
+
+// Question Number 02
+
+var colors = ["orange", "black", "blue", "red", "yellow"];
+console.log(colors);
+
+colors.unshift("pink");
+console.log(colors);
+
+// Question Number 03
+
+var names = ["mahammad", "Ali", "fatima", "hasan", "hussain"];
+console.log(names);
+
+names.push("zainulabideen");
+console.log(names);
+
+// Question Number 04
+
+
+var colors = ["orange", "black", "blue", "red", "yellow"];
+console.log(colors);
+
+colors.unshift("white","grey");
+console.log(colors);
+
+// Question Number 05
+
+
+var colors = ["orange", "black", "blue", "red", "yellow"];
+console.log(colors);
+
+colors.shift();
+console.log(colors);
+
+// Question Number 06
+
+
+var colors = ["orange", "black", "blue", "red", "yellow"];
+console.log(colors);
+
+colors.pop();
+console.log(colors);
+
+
+// Question Number 07
+
+var colors = ["orange", "black", "blue", "red", "yellow"];
+console.log(colors);
+
+colors.splice(2,1,"white");
+console.log(colors);
+
+// Question Number 08
+
+
+var colors = ["orange", "black", "blue", "red", "yellow"];
+console.log(colors);
+
+colors.splice(3);
+console.log(colors);
+
+
+// Question Number 09
+
+var string = "Hyder islam";
+console.log(string);
+
+var newstring = string.replace('islam','abad');
+console.log(newstring);
+
+var c = "Islamabad";
+console.log(c);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
